c5c47fa534b05744534e3c67b65fca64f37028f0
Bino Carlos <bino.carlos@gmail.com>
2012-09-18 01:53:03 +0100

    2nd Layer warehouse working
    and it's soooo simple

--- examples/json_supplier.js
+++ examples/json_supplier.js
@@ -1,52 +1,118 @@
 var io = require('../lib/quarryio');
+var async = require('async');
 
 var root_supplier = null;
 
-function ensureRootSupplier(ready_callback){
+function supplyChainFactory(type, options, ready_callback){
+	if(!io[type]){
+		var ret = io.ram();
+		ready_callback(ret);
+		return ret;
+	}
+	else{
+		return io[type](options, ready_callback);
+	}
+}
 
+function ensureRootSupplier(ready_callback){
 	if(root_supplier){
 		ready_callback && ready_callback(root_supplier);
 		return;
 	}
 
-	root_supplier = io.json_file({
+	supplyChainFactory('json_file', {
 		file:__dirname + '/country.json',
 		pretty:true
 	}, ready_callback);
 }
 
 // new warehouse pointing to a JSON file
-io.warehouse(function(message, callback){
+io.warehouse(function(req, callback){
 
 	// get the root supply chain
 	ensureRootSupplier(function(root_supply_chain){
 
-		root_supply_chain(message, callback);
 
-		/*
-		console.log('have root');
-		console.dir(message);
+		if(req.action=='contract'){
+			
+			root_supply_chain({
+				action:'select',
+				message:{
+					selector:req.message.context	
+				}
+			}, function(error, res){
+				var results = io.container(res.results);
 
-		if(message.action=='contract'){
-			root_supply_chain(message, function(error, res){
-				console.log('-------------------------------------------');
-				console.log('here');
-				console.dir(res);
-			});
+				var remaining_ids = [];
+
+				var fanout_functions = [];
+
+				var fanout_results = [];
+
+				results.each(function(child){
+					if(child.match('supplier')){
+						fanout_functions.push(function(next){
+
+							console.log('-------------------------------------------');
+							console.log('-------------------------------------------');
+							console.log('Trying 2nd layer');
+
+							supplyChainFactory(child.attr('supply_chain'), child.attr(), function(branch_supply_chain){
+								branch_supply_chain({
+									action:'select',
+									message:{
+										selector:req.message.selector
+									}
+								},function(error, res){
+									// we have the raw results
+									console.log('-------------------------------------------');
+									console.log('-------------------------------------------');
+									console.log('HAVE 2nd LAYER RESULTS');
+									console.dir(res);
+									fanout_results = fanout_results.concat(res.results);
+									next();
+								})
+							})
+						})
+					}
+					else{
+						remaining_ids.push(child.quarryid());
+					}
+				})
+
+				// append the default selector function
+				// this is the context ids striped of the suppliers
+				fanout_functions.push(function(next){
+					root_supply_chain({
+						action:'select',
+						message:{
+							selector:req.message.selector,
+							previous:remaining_ids
+						}
+					},function(error, res){
+						fanout_results = fanout_results.concat(res.results);
+						next();
+					})
+				})
+
+				async.parallel(fanout_functions, function(){
+					console.log('-------------------------------------------');
+					console.log('finished');
+					console.dir(fanout_results);
+				})
+
+			})
 		}
 		// this is destined for another supply_chain
-		else if(message.pointer){
+		else if(req.pointer){
 
 		}
 		// a default one - pipe normally
 		else{
-			root_supply_chain(message, function(){
-				console.log('-------------------------------------------');
-				console.log('here');
-			});
+			root_supply_chain(req, callback);
 		}
 
-		*/
+
 	})
 	
 })
@@ -58,7 +124,7 @@ io.warehouse(function(message, callback){
 		})
 		*/
 
-		warehouse('area', '.cool').when(function(result){
+		warehouse('fruit', '.food').when(function(result){
 			console.log('-------------------------------------------');
 			console.log(result.count());
 		})
