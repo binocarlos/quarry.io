d5ceb8827187d641b7cf6f11ed422d474eacd132
Bino Carlos <bino.carlos@gmail.com>
2013-01-01 13:24:46 +0000

    Added append test

--- test/warehouse.js
+++ test/warehouse.js
@@ -47,4 +47,80 @@ describe('warehouse', function(){
 
 	})
 
+	it('should append things', function(done){
+
+		//console.log = function(){}
+		eyes.inspect = function(){}
+		var old_log = console.log;
+		console.log = function(){}
+
+		var folder = __dirname + '/fixtures/tmp/xmlfiles/';
+		var livefile = folder + 'cities.xml';
+		var backupfile = folder + 'cities2.xml';
+
+		var content = fs.readFileSync(backupfile, 'utf8');
+		fs.writeFileSync(livefile, content, 'utf8');
+
+		var supplier_config = {
+			"driver":"ram.file",
+			"directory":__dirname + "/fixtures/tmp/xmlfiles",
+			"path":"cities",
+			"format":"xml"
+		}
+		
+		io
+		.supplier(supplier_config)
+		.ready(function(warehouse){
+
+			warehouse('folder city[name^=b]')
+			
+				.ship(function(cities){
+
+					var area = io.new('area', {
+						name:'Bishopston',
+						population:387
+					}).addClass('rich')
+
+					cities.append(area).ship(function(appended){
+
+						console.log('-------------------------------------------');
+						console.log('-------------------------------------------');
+						console.log('original append');
+						eyes.inspect(appended.toJSON());
+
+						var house = io.new('house', {
+							name:'Big House',
+							population:3
+						}).addClass('grand')
+
+						appended.append(house)
+						.on('error', function(error){
+							console.log('-------------------------------------------');
+							console.log('error');
+							eyes.inspect(error.toJSON());
+							console.log('-------------------------------------------');
+						})
+						.ship(function(houses){
+
+							warehouse('folder city[name^=b] area[name^=Bish] house').ship(function(houses){
+								houses.count().should.equal(2);
+								houses.eq(1).attr('name').should.equal('Big House');
+								console.log = old_log;
+								done();
+							})
+
+						})
+						
+						
+						
+					})
+
+
+
+					
+				})
+		})
+
+	})
+
 })
