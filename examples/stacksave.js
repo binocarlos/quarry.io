02f96ede27ce3cd10c11ca83d3c9c1e903c47d3b
Bino Carlos <bino.carlos@gmail.com>
2013-01-02 18:07:20 +0000

    sorted the tests out

---
+++ examples/stacksave.js
@@ -0,0 +1,54 @@
+var io = require('../');
+var eyes = require('eyes');
+var fs = require('fs');
+
+var folder = __dirname + "/localfiles";
+var network_config = require('./network.json');
+
+network_config.resources.localfiles = folder;
+
+var livefile = folder + '/xmlfiles/cities.xml';
+var backupfile = folder + '/xmlfiles/citiesbranch.xml';
+
+var livefile2 = folder + '/xmlfiles/usacities.xml';
+var backupfile2 = folder + '/xmlfiles/usacities2.xml';
+
+var content = fs.readFileSync(backupfile, 'utf8');
+fs.writeFileSync(livefile, content, 'utf8');
+
+var content2 = fs.readFileSync(backupfile2, 'utf8');
+fs.writeFileSync(livefile2, content2, 'utf8');
+
+
+io
+.network('development', network_config)
+.stack({
+	hostname:'dev.jquarry.com',
+	path:__dirname+'/stack'
+})
+.listen(function(network){
+
+	network.warehouse('dev.jquarry.com', function(warehouse){
+
+		warehouse('folder city[name^=b]')
+			
+			.ship(function(cities){
+
+				cities.attr({
+					'food':'apples'
+				}).addClass('saved');
+
+				cities.save().ship(function(results){
+					console.log('-------------------------------------------');
+					console.log('after save');
+					eyes.inspect(results);
+				})
+
+				
+
+				
+			})
+	})
+	
+	
+})
