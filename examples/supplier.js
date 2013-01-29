7458ffc9e2ba15638f02ac9cf9da0f85f49fd2f0
Bino Carlos <kai@quarry.io>
2013-01-29 18:10:24 +0000

    GET and POST quarryDB

--- examples/supplier.js
+++ examples/supplier.js
@@ -1,5 +1,6 @@
 var io = require('../');
 var eyes = require('eyes');
+var async = require('async');
 
 var supplier = io.supplier.quarrydb({
 	'collection':'testing123'
@@ -9,19 +10,47 @@ var child = io.new('product', {
 	price:100
 })
 
-var req = io.network.request({
-	method:'post',
-	body:child.toJSON()
-})
+async.series([
+	function(next){
+		var req = io.network.request({
+			method:'post',
+			body:child.toJSON()
+		})
 
-var res = io.network.response();
+		var res = io.network.response();
 
-res.on('send', function(){
-	console.log('-------------------------------------------');
-	console.log('-------------------------------------------');
-	eyes.inspect(res.toJSON());
-})
+		res.on('send', function(){
+			console.log('-------------------------------------------');
+			console.log('-------------------------------------------');
+			console.log('after POST');
+			eyes.inspect(res.toJSON());
+			next();
+		})
 
-supplier(req, res, function(){
-	res.send('not found');
-})
+		supplier(req, res, function(){
+			res.send('not found');
+		})
+	},
+
+	function(next){
+		var req = io.network.request({
+			method:'get',
+			url:'/?q=product'
+		})
+
+		var res = io.network.response();
+
+		res.on('send', function(){
+			console.log('-------------------------------------------');
+			console.log('-------------------------------------------');
+			console.log('after GET');
+			eyes.inspect(res.toJSON());
+			next();
+		})
+
+		supplier(req, res, function(){
+			res.send('not found');
+		})
+	}
+])
+
