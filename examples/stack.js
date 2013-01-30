22c2df24c92e95ec7ed45827de355d134620f772
Bino Carlos <kai@quarry.io>
2013-01-30 14:06:26 +0000

    Stack data building

--- examples/stack.js
+++ examples/stack.js
@@ -1,33 +1,87 @@
 var io = require('../');
 var eyes = require('eyes');
-var fs = require('fs');
+var async = require('async');
+var utils = require('../lib/utils');
 
-io
-.network('development', {
-	folder:__dirname+'/networks/basic'
-})
-.boot(function(network){
+/*
 
-	/*
-	console.log('-------------------------------------------');
-	console.log('-------------------------------------------');
-	console.log('network is ready');
-
-	network.warehouse('dev.jquarry.com', function(warehouse){
-
-		console.log('-------------------------------------------');
-		console.log('-------------------------------------------');
-		console.log('stack is ready');
-
-		warehouse('folder city[name^=b]').ship(function(cities, errors){
-			console.log('-------------------------------------------');
-			console.log('-------------------------------------------');
-			console.log('cities is loaded');
-			eyes.inspect(cities.toJSON());
-			//eyes.inspect(errors);
-			
-		})
-	})
-	*/
+	make some test data
 	
-})
+*/
+var data = '<product price="100"><caption name="testing123"/></product>';
+var childdata = '<food name="othertest"/>';
+
+var container = io.new(data);
+var child = io.new(childdata);
+
+container.append(child);
+
+/*
+
+	make the stack from container data
+	
+*/
+
+var stackdata = {
+	
+	/*
+	
+		the core stack configuration
+		
+	*/
+	attr:{
+		name:"My First Test Stack"	
+	},
+	/*
+	
+		database is a single container representing
+		a supplier
+		
+	*/
+	database:{
+		"module":"io.supplier.quarrydb",
+		"config":{
+			"collection":"system"
+		}
+	},
+	/*
+	
+		API contains all of the endpoints within the stack
+		
+	*/
+	api:{
+		attr:{
+			
+		},
+		endpoints:[{
+			"route":"/",
+			"module":"io.supplier.quarrydb",
+			"config":{
+				"collection":"testing123"
+			}
+		}]
+	},
+	/*
+	
+		contains all of the websites hosted within this stack
+		
+	*/
+	webserver:{
+		attr:{
+			
+		},
+		virtualhosts:[{
+			"name":"Simples",
+			"hostnames":[
+				"dev.jquarry.com",
+				"localhost:8080",
+				"localhost"
+			],
+			"document_root":__dirname + '/fixtures/websites/simples/www'
+		}]
+	}
+}
+
+var teststack = io.stack(stackdata);
+
+console.log(JSON.stringify(teststack.toJSON(), null, 4));
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+/*
+  Copyright (c) 2012 All contributors as noted in the AUTHORS file
+
+  This file is part of quarry.io
+
+  quarry.io is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  quarry.io is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+
+	quarry.io main API expose
+	
+*/
 
 
 module.exports = {
@@ -7,9 +31,7 @@ module.exports = {
 	warehouse:require('./lib/warehouse'),
 	reception:require('./lib/reception'),
 	network:require('./lib/network'),
-	supplier:{
-		container:require('./lib/supplier/container'),
-		rest:require('./lib/supplier/rest'),
-		quarrydb:require('./lib/supplier/quarrydb')
-	}
+  stack:require('./lib/stack'),
+	supplier:require('./lib/supplier'),
+	middleware:require('./lib/supplier').middleware
 }
