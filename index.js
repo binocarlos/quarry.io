e35499e2a147f80a261259b3d2bf10414a8c9ca6
Bino Carlos <kai@quarry.io>
2013-02-05 10:22:01 +0000

    Getting stacks to load

--- index.js
+++ index.js
@@ -17,6 +17,10 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+var bootstrap = require('./lib/tools/bootstrap');
+
+bootstrap();
+
 /*
 
 	quarry.io main API expose
